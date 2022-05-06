b648adc03a91bae4ca2d7d1d39c2ac60aced1eba
FabioRocha231 <fabioharoldo1@gmail.com>
2022-05-05 23:54:11 -0300

    fix: some minimal changes

--- src/controllers/createFeedbackController.ts
+++ src/controllers/createFeedbackController.ts
@@ -1,13 +1,16 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { ICreateFeedbackController } from "../core/protocols/controllers/createFeedbackController";
-import { CreateFeedbackService } from "../services/CreateFeedbackController";
-
+import { CreateFeedbackService } from "../services/createFeedbackService";
 
 export class CreateFeedbackController implements ICreateFeedbackController {
-  async handle(req: Request, res: Response): Promise<Response> {
-    const {type, comment, screenshot} = req.body
-    const db = new CreateFeedbackService()
-    await db.save(type, comment, screenshot)
-    return res.json({message: "Post created"})
+  async handle(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<Response | void> {
+    const { type, comment, screenshot } = req.body;
+    const db = new CreateFeedbackService();
+    await db.save(type, comment, screenshot);
+    next();
   }
 }
--- src/core/validators/CreateFeedbackValidator.ts
+++ src/core/validators/CreateFeedbackValidator.ts
@@ -7,13 +7,13 @@ export class FeedbackValidator implements ICreateFeedbackValidator {
   validate(req: Request, res:Response, next: NextFunction ): Response | void {
     const {type, comment, screenshot} = req.body
     if(!type && !comment) 
-      return res.status(400).json({message: "Type and comment are required"})
+      return res.status(406).json({message: "Type and comment are required"})
     
     if(!type) 
-      return res.status(400).json({message: "Type is required"})
+      return res.status(406).json({message: "Type is required"})
       
     if(!comment) 
-      return res.status(400).json({message: "Comment is required"})
+      return res.status(406).json({message: "Comment is required"})
     next()
   }
 }
